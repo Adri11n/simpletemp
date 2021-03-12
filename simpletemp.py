fcaa355a979c05ae82937465aa46b1090ba244f4
Adri11n <adriaanvanvliet2005@gmail.com>
2021-03-12 22:09:03 +0100

    lol

--- simpletemp.py
+++ simpletemp.py
@@ -4,7 +4,6 @@ import simplelib.collectionvitallibs as extrafunc
 from datetime import date
 from datetime import datetime
 
-os.chdir(os.path.dirname(__file__))
 with open('config.json') as config_file:
     data = json.load(config_file)
 question = data['question']
